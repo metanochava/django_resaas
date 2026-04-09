3ef3df69fc1f6adedc43afc7305d6dcadadcfc1c
Metano Chavana <metanochava@gmail.com>
2026-04-09 18:39:13 +0200

    bump version 0.0.91

--- src/django_resaas/data/entidade/views/site.py
+++ src/django_resaas/data/entidade/views/site.py
@@ -1,60 +1,54 @@
-from django.db import transaction
-from django.contrib.auth.models import Group
 from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
 
 from django_resaas.models.tipo_entidade import TipoEntidade
 from django_resaas.models.entidade import Entidade
-from django_resaas.models.sucursal import Sucursal
-from django_resaas.models.entidade_user import EntidadeUser
-from django_resaas.models.sucursal_user import SucursalUser
-from django_resaas.models.sucursal_user_group import SucursalUserGroup
+from django_resaas.data.entidade.serializers.entidade import EntidadeSerializer
 
-from django_resaas.models.theme import Theme, Typography
-from django_resaas.models.layout_setting import LayoutSetting, AnimationSetting
-from django_resaas.data.theme.serializers.theme import ThemeSerializer, TypographySerializer
-from django_resaas.data.layout_setting.serializers.layout_setting import LayoutSettingSerializer, AnimationSettingSerializer
-
-from django_resaas.core.utils.translate import Translate
 from django_resaas.core.utils import all
 
+
 class SiteAPIView(APIView):
+
     def get(self, request):
-        entidade = None
         origin = request.headers.get("Origin")
-      
-        entidade = Entidade.objects.filter(site=origin).first()
+
+        entidade = (
+            Entidade.objects
+            .select_related(
+                "theme",
+                "typography",
+                "layout_settings",
+                "animation_settings",
+                "tipo_entidade__theme",
+                "tipo_entidade__typography",
+                "tipo_entidade__layout_settings",
+                "tipo_entidade__animation_settings",
+            )
+            .filter(site=origin)
+            .first()
+        )
 
         if not entidade:
-            return all(request, Origin = "Desconhecida")
-        
-        if entidade.theme:
-            theme = Theme.objects.get(id=entidade.theme.id).to_dict()
-        else:
-            tipoentidade = TipoEntidade.objects.get(id=entidade.tipo_entidade.id )
-            theme = Theme.objects.get(id=tipoentidade.theme.id).to_dict()
+            return all(request, Origin="Desconhecida")
 
-        if entidade.typography:
-            typography = Typography.objects.get(id=entidade.typography.id).to_dict()
-        else:
-            tipoentidade = TipoEntidade.objects.get(id=entidade.tipo_entidade.id )
-            typography = Typography.objects.get(id=tipoentidade.typography.id).to_dict()
+        tipo = entidade.tipo_entidade
 
-        if entidade.layout_settings:
-            layout_settings = LayoutSetting.objects.get(id=entidade.layout_settings.id).to_dict()
-        else:
-            tipoentidade = TipoEntidade.objects.get(id=entidade.tipo_entidade.id )
-            layout_settings = LayoutSetting.objects.get(id=tipoentidade.layout_settings.id).to_dict()
-        
+        # ------------------------
+        # 🔥 FALLBACK SYSTEM
+        # ------------------------
+        theme = (entidade.theme or tipo.theme)
+        typography = (entidade.typography or tipo.typography)
+        layout_settings = (entidade.layout_settings or tipo.layout_settings)
+        animation_settings = (entidade.animation_settings or tipo.animation_settings)
 
-        if entidade.animation_settings:
-            animation_settings = AnimationSetting.objects.get(id=entidade.animation_settings.id).to_dict()
-        else:
-            tipoentidade = TipoEntidade.objects.get(id=entidade.tipo_entidade.id )
-            animation_settings = AnimationSetting.objects.get(id=tipoentidade.animation_settings.id).to_dict()
-
-        
-       
-        return all(request, layout_settings = layout_settings, theme = theme, animation_settings= animation_settings, typography= typography, entidade= entidade.id )
+        # ------------------------
+        # 🔥 RESPONSE
+        # ------------------------
+        return all(
+            request,
+            layout_settings=layout_settings.to_dict() if layout_settings else None,
+            theme=theme.to_dict() if theme else None,
+            animation_settings=animation_settings.to_dict() if animation_settings else None,
+            typography=typography.to_dict() if typography else None,
+            entidade=EntidadeSerializer(entidade).data
+        )
