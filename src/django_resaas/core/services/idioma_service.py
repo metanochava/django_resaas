51aca16f127b2b8b7076f472e31a162c1b9ffbf4
Metano Chavana <metanochava@gmail.com>
2026-04-20 10:53:03 +0200

    bump version 0.0.143

--- src/django_resaas/core/services/idioma_service.py
+++ src/django_resaas/core/services/idioma_service.py
@@ -1,5 +1,5 @@
 from django_resaas.models.idioma import Idioma
-
+from django.db import IntegrityError
 class IdiomaService:
     """
     Serviço responsável por inicializar os idiomas base do sistema.
@@ -12,11 +12,10 @@ class IdiomaService:
         ("Français", "fr-fr"),
     ]
 
+
+
     @classmethod
     def load_defaults(cls, stdout=None, style=None):
-        """
-        Cria os idiomas padrão se não existirem.
-        """
 
         def out(msg, sty=None):
             if stdout:
@@ -25,11 +24,22 @@ class IdiomaService:
         out(f"\n 🌍 Idiomas padrão", style.MIGRATE_HEADING if style else None)
 
         for nome, code in cls.DEFAULT_IDIOMAS:
-            idioma, created = Idioma.objects.get_or_create(
-                code=code,
-                nome=nome,
-                estado= 1
-            )
+
+            # 🔥 normalizar
+            code = code.lower().strip()
+
+            try:
+                idioma, created = Idioma.objects.get_or_create(
+                    code=code,
+                    defaults={
+                        "nome": nome,
+                        "estado": 1
+                    }
+                )
+            except IntegrityError:
+                # 🔥 fallback seguro
+                idioma = Idioma.objects.get(code=code)
+                created = False
 
             if created:
                 out(f"✔ Idioma criado:\t {idioma.nome} ({idioma.code})",
