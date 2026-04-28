627b2fceed2a08ee5640ffae655729c9b13fb084
Metano Chavana <metanochava@gmail.com>
2026-04-28 19:39:32 +0200

    bump version 0.0.183

--- src/django_resaas/core/utils/group_creator.py
+++ src/django_resaas/core/utils/group_creator.py
@@ -2,34 +2,44 @@ def group_creator(groups=None):
     if groups is None:
         groups = []
 
-    # 🔥 IMPORT LAZY (tudo aqui dentro)
+    # 🔥 IMPORT LAZY
     from django.contrib.auth.models import Group
     from django_resaas.models.tipo_entidade import TipoEntidade
     from django_resaas.models.entidade import Entidade
     from django_resaas.models.tipo_entidade_group import TipoEntidadeGroup
     from django_resaas.models.entidade_group import EntidadeGroup
 
-    # 🔥 criar base
+    # ------------------------------------------------------
+    # 🔥 GARANTE TipoEntidade BASE
+    # ------------------------------------------------------
     tipo_entidade, _ = TipoEntidade.objects.get_or_create(
         nome="SaaS",
+        estado= 1
     )
 
+    # ------------------------------------------------------
+    # 🔥 GARANTE Entidade COM tipo_entidade
+    # ------------------------------------------------------
     entidade, _ = Entidade.objects.get_or_create(
         nome="Mytech",
+        tipo_entidade=tipo_entidade,  # 🔥 FIX CRÍTICO
+        estado= 1
     )
 
-    # 🔥 criar groups
+    # ------------------------------------------------------
+    # 🔥 CRIA GRUPOS
+    # ------------------------------------------------------
     for g in groups:
         group, _ = Group.objects.get_or_create(name=g)
 
         TipoEntidadeGroup.objects.get_or_create(
             tipo_entidade=tipo_entidade,
             group=group,
-            estado = 1
+            defaults={"estado": 1}
         )
 
         EntidadeGroup.objects.get_or_create(
             entidade=entidade,
             group=group,
-            estado = 1
+            defaults={"estado": 1}
         )
