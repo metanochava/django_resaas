ea3bb4ae3452dda53e253f9645aee8294bdec23c
Metano Chavana <metanochava@gmail.com>
2026-05-11 21:25:35 +0200

    bump version 0.0.221

--- src/django_resaas/models/entity_type_group.py
+++ src/django_resaas/models/entity_type_group.py
@@ -10,7 +10,7 @@ class EntityTypeGroup(TimeModel):
     )
 
     group = models.ForeignKey(
-        "auth.Group",
+        "django_resaas.Group",
         on_delete=models.CASCADE,
         related_name="group_entity_types"
     )
