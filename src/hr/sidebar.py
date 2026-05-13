6951fee67817f41a365f320f3d12505f5817687c
Metano Chavana <metanochava@gmail.com>
2026-05-13 17:41:21 +0200

    bump version 0.0.246

--- src/hr/sidebar.py
+++ src/hr/sidebar.py
@@ -8,4 +8,13 @@ SUBMENUS = [
         "role": "view_hr_dashboard",
         "route": "view_hr_dashboard",
     },
+    {
+        "menu": "Employee",
+        "icon": "person",  # 🔥 segurança
+        "role": "list_employee",
+        "route": "list_employee",
+        "add_role": "add_employee",
+        "add_route": "add_employee",
+        # 'crud': { 'module': 'auth', 'model': 'employee' }
+    },
 ]
