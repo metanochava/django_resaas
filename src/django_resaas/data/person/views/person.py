edce869c948decf7dcc6454b73d20e5482087b43
Metano Chavana <metanochava@gmail.com>
2026-05-15 16:58:40 +0200

    bump version 0.0.277

--- src/django_resaas/data/person/views/person.py
+++ src/django_resaas/data/person/views/person.py
@@ -6,10 +6,11 @@ from rest_framework import filters
 from rest_framework import status
 from rest_framework import viewsets
 from django_resaas.models.person import Person
+from django_resaas.core.base.views import BaseAPIView
 
 from django_resaas.data.person.serializers.person import PersonSerializer
 
-class  PersonAPIView(viewsets.ModelViewSet):
+class  PersonAPIView(BaseAPIView):
 
     filter_backends = (filters.SearchFilter,)
     
