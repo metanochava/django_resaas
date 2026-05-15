929b07d44f859d58c177cfb42700347f955f8ea7
Metano Chavana <metanochava@gmail.com>
2026-05-15 20:46:43 +0200

    bump version 0.0.285

--- src/django_resaas/data/person/serializers/person.py
+++ src/django_resaas/data/person/serializers/person.py
@@ -5,19 +5,19 @@ from rest_framework import serializers
 from django_resaas.models.person import Person
 from django_resaas.core.base.serializers import BaseSerializer
 from django_resaas.data.user.serializers.user import UserSerializer
-
+ 
 class PersonSerializer(BaseSerializer):
     user_data = UserSerializer(source='user', read_only=True)
     me = serializers.SerializerMethodField()
 
     class Meta:
         model = Person
-        fields = "__all__"
+        fields = "__all__" 
 
     def get_me(self, obj):
         if not obj.user:
             return None
-        return UserSerializer(obj.user).data['perfil']
+        return UserSerializer(obj.user).data
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
