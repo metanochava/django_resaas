f7172d876cbf44c1315256186c9bc03fc0498a6d
Metano Chavana <metanochava@gmail.com>
2026-05-28 23:41:21 +0200

    bump version 0.0.324

--- src/django_resaas/core/base/serializers.py
+++ src/django_resaas/core/base/serializers.py
@@ -16,3 +16,25 @@ class BaseSerializer(
     permanent_fields_files = []
     label_field = None
     value_field = "id"
+
+    DEFAULT_READ_ONLY_FIELDS = (
+        "id",
+        "entity",
+        "branch",
+        "created_by",
+        "updated_by",
+        "created_at",
+        "updated_at",
+        "deleted_at",
+    )
+
+    def get_fields(self):
+
+        fields = super().get_fields()
+
+        for name in self.DEFAULT_READ_ONLY_FIELDS:
+
+            if name in fields:
+                fields[name].read_only = True
+
+        return fields
