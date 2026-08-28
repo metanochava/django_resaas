37abe9409cb2ca17ee234218b81d92668e2fac41
Metano Chavana <metanochava@gmail.com>
2026-08-28 16:35:03 +0200

    bump version 0.0.445

--- src/hr/urls.py
+++ src/hr/urls.py
@@ -1,12 +1,12 @@
 
 from django.urls import path, include
 from rest_framework import routers
-from hr.views.employee import EmployeeAPIView
+# from hr.views.employee import EmployeeAPIView
 
 
 router = routers.DefaultRouter()
-router.register("employees", EmployeeAPIView, basename="employees")
+# router.register("employees", EmployeeAPIView, basename="employees")
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("hr", include(router.urls)),
 ]
