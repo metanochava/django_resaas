ca55a5423ca12e6be6bab835c637fdead8e6cac1
Metano Chavana <metanochava@gmail.com>
2026-09-01 12:37:17 +0200

    bump version 0.0.457

--- src/django_resaas/data/person/views/person.py
+++ src/django_resaas/data/person/views/person.py
@@ -6,10 +6,11 @@ from rest_framework import filters
 from rest_framework import status
 from rest_framework import viewsets
 from django_resaas.models.person import Person
-from django_resaas.core.base.views import BaseAPIView
+from django_resaas.core.base.views import BaseAPIView, registerView
 
 from django_resaas.data.person.serializers.person import PersonSerializer
 
+@registerView('persons')
 class  PersonAPIView(BaseAPIView):
     serializer_class = PersonSerializer
     queryset = Person.objects.all()
